8d6bfcdfaee9bbcd4603a9a4f0c116b583f1d7ea
Vasyl Hordieiko <pemiker@gmail.com>
2022-04-13 20:01:53 +0300

    new second

--- src/main/java/com/hord/Main.java
+++ src/main/java/com/hord/Main.java
@@ -3,5 +3,7 @@ package com.hord;
 public class Main {
     public static void main(String[] args) {
         String first = "first";
+        String second = "second";
+        String third = "third";
     }
 }
