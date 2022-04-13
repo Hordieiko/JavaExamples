118593c75c66fe02b2898ff0228f537e5c1eeb69
Vasyl Hordieiko <pemiker@gmail.com>
2022-04-13 21:46:58 +0300

    m2

--- src/main/java/com/hord/Main.java
+++ src/main/java/com/hord/Main.java
@@ -2,12 +2,13 @@ package com.hord;
 
 public class Main {
     public static void main(String[] args) {
-        String first = "first";
         String first2 = "first";
-        String first1 = "first";
-        String first3 = "first";
+        String first = "first";
         String first4 = "first";
+        String first1 = "first";
         String first5 = "first";
-        System.out.println("m1");
+        //asdas
+        String first3 = "first";
+        System.out.println("m2");
     }
 }
